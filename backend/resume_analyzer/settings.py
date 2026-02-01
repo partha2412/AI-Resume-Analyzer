7435c5afdd1dc030e92313a6ddf0f8230175747a
Partha Singh <parthasingh35@gmail.com>
2026-02-01 08:53:49 +0530

    mySql init

--- backend/resume_analyzer/settings.py
+++ backend/resume_analyzer/settings.py
@@ -27,6 +27,15 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(BASE_DIR / ".env")
+
+
 
 # Application definition
 
@@ -82,11 +91,23 @@ WSGI_APPLICATION = 'resume_analyzer.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": os.getenv("DB_ENGINE"),
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
+        "OPTIONS": {
+            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
+            "charset": "utf8mb4",
+        },
     }
 }
+
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
 
 
 # Password validation
